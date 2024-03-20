a0676834de2783dea4e6257f7779d3fd51e4319d
Karwai Kang <43213181+KarwaiK@users.noreply.github.com>
2024-03-20 11:34:26 -0400

    Add custom mouse cursor reticles

--- main.gd
+++ main.gd
@@ -1,12 +1,21 @@
 ## Contributors: Karwai
-
+class_name Main
 extends Node
 ## The main scene, holding all other nodes.
 
-## Change main viewport cull mask to hide sprite intended only for minimap.
+## Size of a reticle sprite image in pixels.
+const RETICLE_SIZE: int = 64
+## Number of reticle sprite images in the reticle atlas.
+const RETICLE_COUNT: int = 4
+## Reticle sprite atlas.
+static var reticle: Texture2D = load("res://ui/reticle.png")
+
 func _ready() -> void:
+	# Change main viewport cull mask to hide sprite intended only for minimap.
 	var viewport: Viewport = get_viewport()
 	viewport.canvas_cull_mask = 4294967291
+	# Use the first reticle.
+	set_reticle(0)
 
 ## Create a new game instance and listen for restart/quit events.
 func start_game() -> void:
@@ -30,3 +39,25 @@ func _on_main_menu_settings_menu_opened() -> void:
 	if settings_instance != null:
 		add_child(settings_instance)
 		settings_instance.settings_menu_closed.connect(show_main_menu)
+
+
+## Get an individual reticle image from the reticle atlas image.
+static func get_reticle_image(index: int) -> Resource:
+	var atlas: AtlasTexture = AtlasTexture.new()
+	atlas.set_atlas(reticle)
+	var region_rect: Rect2 = Rect2(
+			index * RETICLE_SIZE, 0, RETICLE_SIZE, RETICLE_SIZE
+	)
+	atlas.set_region(region_rect)
+	return atlas
+
+
+## Change the mouse cursor's appearance to a reticle.
+static func set_reticle(type: int) -> void:
+	var image = get_reticle_image(type)
+	var hotspot: Vector2 = Vector2(RETICLE_SIZE / 2, RETICLE_SIZE / 2)
+	Input.set_custom_mouse_cursor(
+			image,
+			Input.CursorShape.CURSOR_ARROW,
+			hotspot
+	)
