9be2d7941c71333b90dba7d9781196dced175bea
jamcre <jmcrespo123@gmail.com>
2024-04-08 00:50:35 -0400

    Skull Enemy Update - Tombstone's healthbar visual fix - added Ice Shield to Frost Skull. Unless the player is within a certain range, the Frost Skull is enveloped in an Ice Shield and becomes invulnerable

--- enemy/skulls/frost_skull.gd
+++ enemy/skulls/frost_skull.gd
@@ -1,36 +1,47 @@
 extends Skull
 
-var player_distance_threshold = 100
+# Constants
+var player_distance_threshold = 85
+
+# Variables
 var will_shoot = false
 var frost_projectile = preload("res://projectile/frost_projectile.tscn")
 
 func _ready() -> void:
+	# Initialize properties
 	health = 50
 	speed = 25
 
 func _physics_process(_delta: float) -> void:
-	if player and awake: ## RE-ADD AWAKE CONDITION AFTER TESTING
+	if player and awake:
+		# Calculate direction and distance to player
 		var direction: Vector2 = global_position.direction_to(player.global_position)
 		var distance_to_player = global_position.distance_to(player.global_position)
 
-		if distance_to_player <= player_distance_threshold and not will_shoot:
-			# Stop moving immediately
-			velocity = Vector2.ZERO
-			print("Timer Start, Will Shoot")
-			$Shoot.start()
+		if distance_to_player <= player_distance_threshold:
+			# Player is within shooting range
 			will_shoot = true
-		elif distance_to_player > player_distance_threshold and will_shoot:
-			# Player moved outside threshold before shooting, reset will_shoot
+		elif will_shoot:
+			# Player moved out of range, reset shooting flag
 			will_shoot = false
 
-		if not will_shoot:
-			# Move towards the player if not preparing to shoot
+		if will_shoot:
+			# Prepare to shoot
+			velocity = Vector2.ZERO
+			print("Preparing to shoot")
+			$Shoot.start()  # Start shooting timer
+			$Ice.visible = true  # Make Ice Shell visible
+			$CollisionPolygon2D.disabled = true  # Disable collision
+		else:
+			# Move towards the player
 			velocity = direction * speed
 			move_and_slide()
+			$Ice.visible = false  # Hide Ice Shell
+			$CollisionPolygon2D.disabled = false  # Enable collision
 	else:
 		pass
 
 func _on_shoot_timeout():
-	print("Timer Ended, Shoot Frost")
-	## SHOOT LOGIC GOES HERE
-	will_shoot = false
+	print("Shooting Frost")
+	# Shoot logic goes here
+	will_shoot = false  # Reset shooting flag
