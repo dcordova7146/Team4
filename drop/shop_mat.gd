49c580e18e4163c5d8f8a09a5b6ab7149e593a1d
dcordova7146 <diego.cordova79@myhunter.cuny.edu>
2024-05-06 00:30:32 -0400

    ...
    -starting the shop room insides
    -created particles that show item rarity when they spawn

---
+++ drop/shop_mat.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var left: Marker2D = $common_item1
+@onready var mid: Marker2D = $common_item2
+@onready var right: Marker2D = $special_item
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	
+
+
