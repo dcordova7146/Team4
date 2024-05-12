4d4ff058a4e39be08f18996c423b25240730461a
dcordova7146 <diego.cordova79@myhunter.cuny.edu>
2024-05-12 18:14:07 -0400

    Modified the database
    -scrapped some items

--- database/SQLiteController.gd
+++ database/SQLiteController.gd
@@ -42,34 +42,35 @@ func createShopTables() -> void:
 func populateTable() -> void:
 	## common items
 	insertData("Vitamins","2% increase damage Stackable",3,"Common","res://tempAssets/drops/items/tile020.png")
-	insertData("Carrot Berry","range increase on pistol 10% stackable",3,"Common","res://tempAssets/drops/items/tile000.png")
-	insertData("Dangle Berries","last 2 bullets on pistol deal 30% extra damage stackable",3,"Common","res://tempAssets/drops/items/tile001.png")
+	#insertData("Carrot Berry","range increase on pistol 10% stackable",3,"Common","res://tempAssets/drops/items/tile000.png")
+	#insertData("Dangle Berries","last 2 bullets on pistol deal 30% extra damage stackable",3,"Common","res://tempAssets/drops/items/tile001.png")
 	insertData("Square Cherry","increase projectile size on pistol by 10% stackable",5,"Common","res://tempAssets/drops/items/tile003.png")
 	insertData("Prickle Berry","increase pierce on pistol by 1 stackable",5,"Common","res://tempAssets/drops/items/tile005.png")
 	insertData("Gamer Fuel","5% increase to move speed",5,"Common","res://tempAssets/drops/items/tile037.png")
-	insertData("Bad4Us","5% increase to Reload speed + 5% increase to fire rate -5% damage stackable",2,"Common","res://tempAssets/drops/items/tile022.png")
+	insertData("Bad4Us","5% increase to Reload speed and -5% damage stackable",2,"Common","res://tempAssets/drops/items/tile022.png")
 	insertData("Shower in a Can","Fully heal hearts take 10% extra damage from enemies",1,"Common","res://tempAssets/drops/items/tile021.png")
-	insertData("Starberry Leaf","10% increase to sword damage",5,"Common","res://tempAssets/drops/items/tile009.png")
+	insertData("Starberry Leaf","10% increase to all weapon damage",10,"Common","res://tempAssets/drops/items/tile009.png")
 	## uncommon items
-	insertData("Starberries","duplicate current pistol-berry mods onto burst rifle ",10,"Uncommon","res://tempAssets/drops/items/tile019.png")
-	insertData("Jala Fruit","duplicate current pistol-berry mods onto machine gun",10,"Uncommon","res://tempAssets/drops/items/tile014.png")
-	insertData("Muhnanah","duplicate current pistol-berry mods onto shotgun",10,"Uncommon","res://tempAssets/drops/items/tile013.png")
-	insertData("Atomic Fruit","duplicate current pistol-berry mods onto sniper",10,"Uncommon","res://tempAssets/drops/items/tile016.png")
+	#insertData("Starberries","duplicate current pistol-berry mods onto burst rifle ",10,"Uncommon","res://tempAssets/drops/items/tile019.png")
+	#insertData("Jala Fruit","duplicate current pistol-berry mods onto machine gun",10,"Uncommon","res://tempAssets/drops/items/tile014.png")
+	#insertData("Muhnanah","duplicate current pistol-berry mods onto shotgun",10,"Uncommon","res://tempAssets/drops/items/tile013.png")
+	#insertData("Atomic Fruit","duplicate current pistol-berry mods onto sniper",10,"Uncommon","res://tempAssets/drops/items/tile016.png")
 	insertData("Artificial Blood","Gain 25 drops of blood",-1,"Rare","res://tempAssets/drops/items/tile067.png")
-	insertData("Ginger","Cleanse negative effect on 1 random held item",10,"Uncommon","res://tempAssets/drops/items/tile059.png")
-	insertData("Firewood","Load pistol with fire bullets - frozen enemies are no longer invulnerable *replaces current element",15,"Uncommon","res://tempAssets/drops/items/tile063.png")
-	insertData("Frozen Shard","Load pistol with ice bullets - slow enemies on hit *replaces current element",3,"Uncommon","res://tempAssets/drops/items/tile068.png")
-	insertData("Save State","Recover from death with 1 heart",30,"Uncommon","res://tempAssets/drops/items/tile043.png")
+	#insertData("Ginger","Cleanse negative effect on 1 random held item",10,"Uncommon","res://tempAssets/drops/items/tile059.png")
+	#insertData("Firewood","Load pistol with fire bullets - frozen enemies are no longer invulnerable *replaces current element",15,"Uncommon","res://tempAssets/drops/items/tile063.png")
+	#insertData("Frozen Shard","Load pistol with ice bullets - slow enemies on hit *replaces current element",3,"Uncommon","res://tempAssets/drops/items/tile068.png")
+	#insertData("Save State","Recover from death with 1 heart",30,"Uncommon","res://tempAssets/drops/items/tile043.png")
 	##Rare Items
-	insertData("Cool Sunnies","\"I Dont need to reload!\" - Auto reload guns on swap",50,"Rare","res://tempAssets/drops/items/tile070.png")
-	insertData("Epic Visor","\"1 gun is enough\" - Gain stacks on a gun per rooms cleared without swapping guns *5% increase to damage per stack reset on gun swap",50,"Rare","res://tempAssets/drops/items/tile049.png")
-	insertData("Hard Hat","\"safe measure\" - Enemies are hurt on touching player 25% off their max health",50,"Rare","res://tempAssets/drops/items/tile055.png")
+	insertData("Cool Sunnies","\"I Dont need to reload!\" - Auto reload Guns",50,"Rare","res://tempAssets/drops/items/tile070.png")
+	#insertData("Epic Visor","\"1 gun is enough\" - Gain stacks on a gun per rooms cleared without swapping guns *5% increase to damage per stack reset on gun swap",50,"Rare","res://tempAssets/drops/items/tile049.png")
+	#insertData("Hard Hat","\"safe measure\" - Enemies are hurt on touching player 25% off their max health",50,"Rare","res://tempAssets/drops/items/tile055.png")
 	insertData("Speeder","\"zoom zoom\" - increase move speed by 30%",50,"Rare","res://tempAssets/drops/items/tile042.png")
-	insertData("VIP Membership","\"VIP access!\" - 25% off all items forever",50,"Rare","res://tempAssets/drops/items/tile040.png")
-	insertData("Black Credit Card","\"Even better than the gold credit card\" - 25% cash back on purchases at all stores *rounded down",50,"Rare","res://tempAssets/drops/items/tile046.png")
+	#insertData("VIP Membership","\"VIP access!\" - 25% off all items forever",50,"Rare","res://tempAssets/drops/items/tile040.png")
+	#insertData("Black Credit Card","\"Even better than the gold credit card\" - 25% cash back on purchases at all stores *rounded down",50,"Rare","res://tempAssets/drops/items/tile046.png")
 	insertData("Berry Scraps","\"5 second rule!\" - gain a heart back on entering a new room",50,"Rare","res://tempAssets/drops/items/tile051.png")
-	insertData("Plan Z","\"Everything in one conveniently sized pill\" - all stats plus 15%",50,"Rare","res://tempAssets/drops/items/tile035.png")
-	insertData("Medicinal Herbs","\"Its medicinal!\" - immune to negative effects from items",50,"Rare","res://tempAssets/drops/items/tile025.png")
+	insertData("Plan Z","\"Everything in one conveniently sized pill\" - all stats plus 1",50,"Rare","res://tempAssets/drops/items/tile035.png")
+	#insertData("Medicinal Herbs","\"Its medicinal!\" - immune to negative effects from items",50,"Rare","res://tempAssets/drops/items/tile025.png")
+	insertData("Hero Hydration","Gain an extra max life Stackable", 25, "Rare", "res://tempAssets/items/tile026.png")
 	
 #Insert data helper function used to populate table with any item
 func insertData(name: String, description: String, cost:int, rarity: String, path: String) -> void:
@@ -102,8 +103,10 @@ func selectRandomArtifact()->Artifact:
 	
 	return ResourceDirectory.get_resource(database.query_result[0]["itemName"])
 	
-	
-	
+func selectArtifact(artifact: Artifact)-> PackedScene:
+	database.query("SELECT * FROM itemTable WHERE itemName = " + artifact.name)
+	var scenename = "res://artifacts/Artifacts/" + database.query_result[0]["itemName"].replace(" ", "_").lower() + ".tscn"
+	return load(scenename)
 	
 	
 	
