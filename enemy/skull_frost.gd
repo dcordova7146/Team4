b8c9d8bc3990c6aebda653b45c6f71bf2b25a756
Karwai Kang <43213181+KarwaiK@users.noreply.github.com>
2024-05-19 19:01:45 -0400

    Change boss and frost skull logic

--- enemy/skull_frost.gd
+++ enemy/skull_frost.gd
@@ -1,11 +1,17 @@
 extends Skull
 
-const PLAYER_DISTANCE_THRESHOLD: int = 85  # Threshold distance for player detection
-const MAX_HEALTH: int = 100  # Maximum health points
-
-var will_shoot: bool = false  # Flag to indicate if the skull will shoot
-var is_shoot_preparing: bool = false  # Flag to track shoot preparation
-var frost_projectile: Resource = preload("res://projectile/frost_projectile.tscn")  # Preloaded frost projectile
+## Threshold distance for player detection.
+const PLAYER_DISTANCE_THRESHOLD: int = 85
+## Maximum health points.
+const MAX_HEALTH: int = 100
+## Amount to heal by.
+var healed_amount: int = 25
+## Flag to indicate if the skull will shoot.
+var will_shoot: bool = false
+## Flag to track shoot preparation.
+var is_shoot_preparing: bool = false
+## Frost projectile scene.
+var frost_projectile: Resource = preload("res://projectile/frost_projectile.tscn")
 var frozen: bool = false
 @onready var shoot_timer: Timer = $ShootTimer
 @onready var ice_sprite: Sprite2D = $Ice
@@ -18,38 +24,42 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	if player and awake:
-		var direction: Vector2 = global_position.direction_to(player.global_position)
+		var direction_to_player: Vector2 = global_position.direction_to(player.global_position)
 		var distance_to_player: float = global_position.distance_to(player.global_position)
 
-		# Check if player is within shooting range
+		# Check if player is within shooting range.
 		will_shoot = distance_to_player <= PLAYER_DISTANCE_THRESHOLD
 
 		if will_shoot and not is_shoot_preparing:
 			is_shoot_preparing = true
 			velocity = Vector2.ZERO
-			print("Preparing to shoot")
-			shoot_timer.start()  # Start shooting timer
-			ice_sprite.visible = true  # Make ice shell visible
-			collision.disabled = true  # Disable collision
+			# Start shooting timer.
+			shoot_timer.start()
+			# Make ice shell visible.
+			ice_sprite.visible = true
+			# Disable collision.
+			collision.disabled = true
 		elif not will_shoot:
-			velocity = direction * speed
+			velocity = direction_to_player * speed
 			move_and_slide()
-			ice_sprite.visible = false  # Hide ice shell
-			collision.disabled = false  # Enable collision
+			# Hide ice shell.
+			ice_sprite.visible = false
+			# Enable collision.
+			collision.disabled = false
 	else:
 		pass
 
+
 func _on_shoot_timeout() -> void:
-	print("Shooting Frost")
 	will_shoot = false
 	is_shoot_preparing = false
-
 	if ice_sprite.visible:
-		var healed_amount: int = 25
 		health += healed_amount
-		health = min(health, MAX_HEALTH)  # Ensure health doesn't exceed maximum
-		print("Healing: ", health)
-		update_health_bar()  # Update health bar visual
+		# Ensure health doesn't exceed maximum
+		health = min(health, MAX_HEALTH)
+		# Update health bar visual.
+		update_health_bar()
+
 
 func update_health_bar() -> void:
 	if health_bar:
--- enemy/skull_queen.gd
+++ enemy/skull_queen.gd
@@ -1,20 +1,44 @@
 extends Skull
-
-const MAX_HEALTH: int = 600  # Maximum health points
-const DESTINATION_THRESHOLD: float = 10.0 # Approximate area of destination
-
-var is_phase_2: bool = false # Flag for Queen 2nd Phase
-var is_phase_3: bool = false # Flag for Queen 3rd Phase
-var begin_movement: bool = false # Flag for Queen wake-up, lets player move before saving location and starting movement.
-var wake_timer_called: bool = false # Flag for timer being called
-var reached_destination_x: bool = false  # Flag to indicate if Queen reached destination Xcord.
-var reached_destination_y: bool = false  # Flag to indicate if Queen reached destination Ycord.
-var reached_final_destination: bool = false # Flag to indicate if Queen reached final destination.
-var target_location: Vector2 = Vector2() # Record player location that Queen will move to.
-
+## Maximum health points.
+const MAX_HEALTH: int = 600
+## Speed during phase 2.
+var PHASE_2_SPEED: float = 1.25
+## Speed during phase 3.
+var PHASE_3_SPEED: float = 1.25 * 1.25
+## Approximate area of destination.
+const DESTINATION_THRESHOLD: float = 10.0
+## Distinct phases. Determines speed and number of skulls spawned.
+enum Phase {
+	_1,
+	_2,
+	_3
+}
+## Current phase.
+var phase: Phase = Phase._1
+## Flag for wake-up. Lets player move before saving location and starting movement.
+var begin_movement: bool = false
+## Flag for timer being called.
+var wake_timer_called: bool = false
+## If reached x-coordinate destination.
+var reached_destination_x: bool = false
+## If reached y-coordinate destination.
+var reached_destination_y: bool = false
+## If reached final destination.
+var reached_final_destination: bool = false
+## Location of player to move to.
+var target_location: Vector2 = Vector2()
+## Names of skulls that can be randomly spawned.
+var skull_types: Array[String] = [
+		"skull_small", "skull_medium", "skull_bomb", "skull_frost"
+]
+## Times how long it sleeps between movements.
 @onready var move_timer: Timer = $MoveTimer
+## Times how long it sleeps when entering the room.
 @onready var wake_timer: Timer = $WakeTimer
+## Collision polygon.
+## Disabled when sleeping, preventing any damage from being taken.
 @onready var collision_polygon: CollisionPolygon2D = $CollisionPolygon2D
+## Sprites for each phase and sleep status.
 @onready var phase_1: Sprite2D = $Phase1
 @onready var phase_1_sleep: Sprite2D = $Phase1Sleep
 @onready var phase_2: Sprite2D = $Phase2
@@ -22,44 +46,40 @@ var target_location: Vector2 = Vector2() # Record player location that Queen wil
 @onready var phase_3: Sprite2D = $Phase3
 @onready var phase_3_sleep: Sprite2D = $Phase3Sleep
 
+
 func _ready() -> void:
-	health = MAX_HEALTH  # Set initial health to maximum
-	speed = 1 # Movement speed
-	super._ready()  # Call base class _ready function
+	# Set initial health to maximum.
+	health = MAX_HEALTH
+	# Set movement speed.
+	speed = 1
+	# Call base class _ready function.
+	super._ready()
 	update_health_bar()
 	collision_polygon.disabled = true
 
+
 func _physics_process(_delta: float) -> void:
-	if player and awake and !wake_timer_called:
-		print("\nWake Timer Started!")
+	if not player or not awake:
+		return
+	if not wake_timer_called:
 		wake_timer.start()
 		wake_timer_called = true 
-	if player and awake and begin_movement: ## RE-ADD AWAKE BOOl
-		change_phases()
-		if !reached_destination_x:
-			# Move towards the target X coordinate
-			if target_location.x < global_position.x:
-				move_local_x(-speed)
-			else:
-				move_local_x(speed)
+	if begin_movement:
+		var direction: Vector2 = (target_location - global_position).sign()
+		if not reached_destination_x:
+			# Move to target x-coordinate.
+			move_local_x(speed * direction.x)
 			# Check if the Queen reached the target X coordinate
 			if abs(target_location.x - global_position.x) < DESTINATION_THRESHOLD:
 				reached_destination_x = true
-				print("Queen reached target X coordinate")
-		elif !reached_destination_y and reached_destination_x:
-			# Move towards the target Y coordinate
-			if target_location.y < global_position.y:
-				move_local_y(-speed)
-			else:
-				move_local_y(speed)
+		elif not reached_destination_y:
+			# Move to target y-coordinate.
+			move_local_y(speed * direction.y)
 			# Check if the Queen reached the target Y coordinate
 			if abs(target_location.y - global_position.y) < DESTINATION_THRESHOLD:
 				reached_destination_y = true
-				print("Queen reached target Y coordinate")
-		elif reached_destination_x and reached_destination_y and !reached_final_destination:
-			print("Queen has arrived at the target location")
+		elif not reached_final_destination:
 			reached_final_destination = true
-			print("Movement Cycle Ends, Move Timer Begins")
 			move_timer.start()
 			change_sprites()
 			collision_polygon.disabled = true
@@ -68,52 +88,45 @@ func _physics_process(_delta: float) -> void:
 	else:
 		pass
 
-func change_phases() -> void:
-	if health <= MAX_HEALTH * 2/3 and !is_phase_2: 
-		is_phase_2 = true
-		phase_1.visible = false
-		phase_2.visible = true
-		speed = speed * 1.25
-	if health <= MAX_HEALTH * 1/3 and is_phase_2 and !is_phase_3:
-		is_phase_3 = true
-		phase_2.visible = false
-		phase_3.visible = true
-		speed = speed * 1.25
 
+## Wake up, start moving, and allow player to damage it.
+func wake() -> void:
+	collision_polygon.disabled = false
+	health_bar.visible = true
+	change_sprites()
+	# Starting moving to player's position.
+	reached_destination_x = false
+	reached_destination_y = false
+	reached_final_destination = false
+	target_location = player.global_position
+	begin_movement = true
+
+
+## Toggle sprite from sleep to not asleep variation.
 func change_sprites() -> void:
-	if health > MAX_HEALTH * 2/3: # Phase 1 Sprite Changes
-		if phase_1.visible:
-			phase_1_sleep.visible = true
-			phase_1.visible = false
-		elif phase_1_sleep.visible:
-			phase_1.visible = true
-			phase_1_sleep.visible = false
-	elif health <= MAX_HEALTH * 2/3 and health > MAX_HEALTH * 1/3: # Phase 2 Sprite Changes
-		if phase_2.visible:
-			phase_2_sleep.visible = true
-			phase_2.visible = false
-		elif phase_2_sleep.visible:
-			phase_2.visible = true
-			phase_2_sleep.visible = false
-	elif health <= MAX_HEALTH * 1/3: # Phase 3 Sprite Changes
-		if phase_3.visible:
-			phase_3_sleep.visible = true
-			phase_3.visible = false
-		elif phase_3_sleep.visible:
-			phase_3.visible = true
-			phase_3_sleep.visible = false
+	if phase == Phase._1:
+		phase_1_sleep.visible = phase_1.visible
+		phase_1.visible = not phase_1.visible
+	elif phase == Phase._2:
+		phase_2_sleep.visible = phase_2.visible
+		phase_2.visible = not phase_2.visible
+	else:
+		phase_3_sleep.visible = phase_3.visible
+		phase_3.visible = not phase_3.visible
 
+
+## Update health bar value.
 func update_health_bar() -> void:
 	if health_bar:
-		health_bar.value = health  # Update health bar value
+		health_bar.value = health
 
-# Function to spawn a single random skull
+
+## Spawn a number of skulls.
 func spawn_random_skull() -> void:
-	var skull_types: Array[String] = ["skull_small", "skull_medium", "skull_bomb", "skull_frost"]
 	var number_of_skulls: int = 0
-	if !is_phase_2 and !is_phase_3: 
+	if phase == Phase._1: 
 		number_of_skulls = 1
-	elif is_phase_2 and !is_phase_3:
+	elif phase == Phase._2:
 		number_of_skulls = 2
 	else:
 		number_of_skulls = 3
@@ -137,10 +150,31 @@ func spawn_random_skull() -> void:
 			enemy_instance.awake = true
 			room.add_enemy(enemy_instance)
 		else:
-			print("Failed to instantiate enemy from path: ", random_skull_path)
+			push_error(
+					"Failed to instantiate enemy from path: ",
+					random_skull_path
+			)
 
 
-func die() -> void: # Emit 3 Hearts, b/c Queen is a Boss
+## Update phase when is damage taken.
+func take_damage(damage_total: float) -> void:
+	super.take_damage(damage_total)
+	if health > MAX_HEALTH * 2/3:
+		pass
+	elif health > MAX_HEALTH * 1/3:
+		phase = Phase._2
+		speed = PHASE_2_SPEED
+		phase_1.visible = false
+		phase_2.visible = true
+	else:
+		phase = Phase._3
+		speed = PHASE_3_SPEED
+		phase_2.visible = false
+		phase_3.visible = true
+
+
+## Drop more loot than normal enemies.
+func die() -> void:
 	var offset: int = 12
 	# Emit events with randomly adjusted positions
 	for i: int in range(12):
@@ -155,28 +189,10 @@ func die() -> void: # Emit 3 Hearts, b/c Queen is a Boss
 	if spawn_count > 0:
 		spawn_enemy(enemy_to_spawn, spawn_count)
 
+
 func _on_move_timer_timeout() -> void:
-	# Reset movement flags and record new target location
-	print("\nMove Timer Ends")
-	reached_destination_x = false
-	reached_destination_y = false
-	reached_final_destination = false
-	print("New Target Location Saved")
-	target_location = player.global_position
-	# Modify sprites and collisions
-	change_sprites()
-	collision_polygon.disabled = false
-	health_bar.visible = true
+	wake()
+
 
 func _on_wake_timer_timeout() -> void:
-	print("\nWake Timer Ends")
-	# Change to Awake Sprite
-	collision_polygon.disabled = false
-	health_bar.visible = true
-	# Save Player Location
-	target_location = player.global_position
-	# Begin Movement Cycle, (set Flag to True)
-	begin_movement = true
-	print("Target Location Saved, Movement Cycle Begins")
-	change_sprites() 
-
+	wake()
